ab6e6359b223bf698f66fed4169ded8ce3300c3b
leecamm <leduchieu642@gmail.com>
2022-04-26 02:20:45 +0200

    fix: header and add heroicons

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,17 +4,17 @@ import { default as logo } from "../static/logo.svg";
 
 const Header = () => {
   return (
-    <div className="flex justify-between">
-      <div className="pt-16">
-        <Image src={logo} alt="logo" width={318} height={106} />
-      </div>
-      <div className="flex gap-x-5 pt-5">
+    <div>
+      <div className="flex gap-x-5 justify-end pt-5">
         <a href="#about">
-          <p className="font-medium text-white">about</p>
+          <a className="font-medium text-white">about</a>
         </a>
         <a href="#contact">
           <p className="font-medium text-white">contact</p>
         </a>
+      </div>
+      <div className="">
+        <Image src={logo} alt="logo" width={318} height={106} />
       </div>
     </div>
   );
