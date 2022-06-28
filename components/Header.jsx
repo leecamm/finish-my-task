7779a78e4512b376e84bcda160e67144d2139567
leecamm <leduchieu642@gmail.com>
2022-06-28 15:35:40 +0200

    feat: about modal

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { default as logo } from "../public/static/logo.svg";
 
+import ModalAbout from "./ModalAbout";
+import IconQuestion from "./icons/IconQuestion";
+
 const Header = () => {
+  const [openAbout, setOpenAbout] = useState(false);
   return (
     <div className="">
-      <div className="flex gap-x-5 md:justify-end pt-5 justify-center">
-        <a href="#about">
-          <p className="font-medium text-white text-sm md:text-base">about</p>
+      <div className="flex gap-x-5 md:justify-end items-center pt-5 justify-center">
+        <a onClick={() => setOpenAbout((value) => !value)}>
+          <p className="font-medium text-white text-sm md:text-base cursor-pointer">
+            about
+          </p>
         </a>
         <a href="#contact">
           <p className="font-medium text-white text-sm md:text-base">contact</p>
         </a>
+        <a href="#contact">
+          <p className="font-medium text-white text-sm md:text-base">
+            <IconQuestion />
+          </p>
+        </a>
       </div>
-      <div className="cursor-pointer logo flex justify-center mx-auto mb-14 md:m-0">
+      <div className="cursor-pointer logo flex justify-center mx-auto mt-5 mb-14 md:m-0">
         <Image src={logo} alt="logo" />
       </div>
+
+      <ModalAbout openAbout={openAbout} setOpenAbout={setOpenAbout} />
     </div>
   );
 };
---
+++ components/ModalAbout.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import IconClose from "./icons/IconClose";
+
+const ModalAbout = ({ openAbout, setOpenAbout }) => {
+  return (
+    <>
+      <div
+        className={`absolute h-full w-full left-0 top-0 bg-black bg-opacity-30 ${
+          openAbout ? "" : "hidden"
+        }`}
+        onClick={() => setOpenAbout(false)}
+      ></div>
+      <div
+        className={`max-w-xl bg-gray-600 absolute w-11/12 h-[30rem] left-1/2 top-1/2 p-5 rounded-lg shadow-lg z-10 tracking-wide overflow-y-scroll no-scrollbar ${
+          openAbout ? "" : "hidden"
+        }`}
+        style={{
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <div className="text-gray-100 flex justify-between items-center font-semibold">
+          <h1>What is Pomodoro technique?</h1>
+          <button type="button" onClick={() => setOpenAbout(false)}>
+            <IconClose />
+          </button>
+        </div>
+        <div className="h-0.5 w-full bg-gray-300 my-4"></div>
+        <div className="flex gap-5">
+          <div className="w-full text-gray-200">
+            The{" "}
+            <a
+              href="https://francescocirillo.com/pages/pomodoro-technique"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sky-400"
+            >
+              Pomodoro Technique
+            </a>{" "}
+            is a time management method developed by Francesco Cirillo in the
+            late 1980s. It uses a kitchen timer to break work into intervals,
+            typically 25 minutes in length, separated by short breaks. Each
+            interval is known as a pomodoro, from the Italian word for tomato,
+            after the tomato-shaped kitchen timer Cirillo used as a university
+            student. -{" "}
+            <a
+              href="https://en.wikipedia.org/wiki/Pomodoro_Technique"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sky-400"
+            >
+              Wikipedia
+            </a>
+          </div>
+        </div>
+        <div className="text-gray-100 flex justify-between items-center font-semibold mt-6">
+          <h1>What is Finish my task website?</h1>
+        </div>
+        <div className="h-0.5 w-full bg-gray-300 my-4"></div>
+        <div className="flex gap-5">
+          <div className="w-full text-gray-200">
+            Finish my task is a customizable pomodoro timer that works on
+            desktop and mobile browsers. This website can help you to stay
+            focused on any task you are working on, such as studying, writing,
+            or coding. You can add tasks, play ambient sounds, and change the
+            website background to bring you the most comfortable working
+            environment.
+          </div>
+        </div>
+        <div className="text-gray-100 flex justify-between items-center font-semibold mt-6">
+          <h1>Features</h1>
+        </div>
+        <div className="h-0.5 w-full bg-gray-300 my-4"></div>
+        <div className="flex gap-5">
+          <div className="w-full text-gray-200">
+            <ul className="list-disc mx-4 space-y-1 ">
+              <li>
+                <b className="text-sky-300">Responsive design</b> that works on
+                both desktop and mobile browsers
+              </li>
+              <li>
+                <b className="text-sky-300">Customizable timer</b> intervals to
+                suit your preference
+              </li>
+              <li>
+                <b className="text-sky-300">Audio notification</b> at the end of
+                a timer period to notify you
+              </li>
+              <li>
+                <b className="text-sky-300">Background ambient sounds</b> to
+                help you stay focused and relaxed
+              </li>
+              <li>
+                <b className="text-sky-300">Beautiful background images</b> to
+                add mood to your work
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex gap-4 my-2">
+          <div
+            className="text-sm underline flex items-center text-gray-300 hover:text-gray-200 mt-5 cursor-pointer"
+            onClick={() => setOpenAbout(false)}
+          >
+            close
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default React.memo(ModalAbout);
