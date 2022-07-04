03af110b00c3fc0cac8929df5f151fb622826d61
leecamm <leduchieu642@gmail.com>
2022-07-04 10:48:05 +0200

    feat: google analytic

--- pages/index.js
+++ pages/index.js
@@ -66,6 +66,17 @@ const Home = () => {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff"></meta>
+
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CJV9YG5ET9"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-CJV9YG5ET9');
+        </script>
       </Head>
       <Header />
       <div className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-10 gap-2 md:gap-6 lg:gap-10">
