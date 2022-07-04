54a409675e1329d1a48c812473a4e6315be264cd
leecamm <leduchieu642@gmail.com>
2022-07-04 10:58:50 +0200

    feat: Google analytic

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import Head from "next/head";
+import Script from "next/script";
 
 import Header from "../components/Header";
 import Pomodoro from "../components/Pomodoro";
@@ -26,67 +27,76 @@ const Home = () => {
   };
 
   return (
-    <div className="px-5 lg:px-36 bg" style={{ ...baseStyle, ...selectedImg }}>
-      <Head>
-        <title>Finish My Task - Not Just A Pomodoro Timer</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="google-site-verification"
-          content="wX2DyPnwhuvTsvcpy1zhyy42l2KXNTx1SbhjXpM7Ufk"
-        />
-        <meta
-          name="keywords"
-          content="pomodoro, pomodoro timer, time management"
-        />
-        <meta
-          name="description"
-          content="A customizable Pomodoro timer that works on desktop and mobile browsers. You can add tasks, play ambient sounds, and change the website background to create the most pleasant working environment."
-        ></meta>
-        <meta property="og:title" content="Finish My Task" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://finishmytask.online" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff"></meta>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-CJV9YG5ET9"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-CJV9YG5ET9');
-        </script>
-      </Head>
-      <Header />
-      <div className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-10 gap-2 md:gap-6 lg:gap-10">
-        <Todo className="order-2 md:order-1" />
-        <Pomodoro
-          handleSelectedImg={handleSelected}
-          className="order-1 md:order-2"
-        />
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <div
+        className="px-5 lg:px-36 bg"
+        style={{ ...baseStyle, ...selectedImg }}
+      >
+        <Head>
+          <title>Finish My Task - Not Just A Pomodoro Timer</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="google-site-verification"
+            content="wX2DyPnwhuvTsvcpy1zhyy42l2KXNTx1SbhjXpM7Ufk"
+          />
+          <meta
+            name="keywords"
+            content="pomodoro, pomodoro timer, time management"
+          />
+          <meta
+            name="description"
+            content="A customizable Pomodoro timer that works on desktop and mobile browsers. You can add tasks, play ambient sounds, and change the website background to create the most pleasant working environment."
+          ></meta>
+          <meta property="og:title" content="Finish My Task" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://finishmytask.online" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
+        </Head>
+        <Header />
+        <div className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-10 gap-2 md:gap-6 lg:gap-10">
+          <Todo className="order-2 md:order-1" />
+          <Pomodoro
+            handleSelectedImg={handleSelected}
+            className="order-1 md:order-2"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
